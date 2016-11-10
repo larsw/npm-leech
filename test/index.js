2673a83b1311caba23757eae1c12ea8af43c234e
Lars Wilhelmsen <lars@sral.org>
2016-11-10 22:02:21 +0100

    0.2.0

--- test/index.js
+++ test/index.js
@@ -1,15 +1,17 @@
 import fs from 'fs'
 import path from 'path'
 import test from 'ava'
-import defaultOpts from '../lib/cli'
+import {defaultOpts} from '../lib/cli'
 import leech from '../lib/leech'
+import rimraf from 'rimraf'
 
-test('dogfooding works', t => {
+test.cb('dogfooding works', t => {
   let opts = Object.assign({}, defaultOpts)
   opts.input = path.join(__dirname, '../package.json')
-  opts.verbose = true
+  rimraf.sync(opts.output)  
   opts.done = () => {
     t.true(fs.existsSync(opts.output))
+    rimraf.sync(opts.output)  
     t.end()
   }
   try {
