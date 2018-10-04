c57947254523c2333b398663a45110b4a123bb46
Lars Wilhelmsen <lars@sral.org>
2018-10-04 10:53:12 +0200

    Upped dependencies. Removed old typings and added npmjs-based @types equivalents

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/archiver/index.d.ts" />
-/// <reference path="globals/async/index.d.ts" />
-/// <reference path="globals/axios/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
